03a2b8b93029a83e08daba0b76dcd7c46799a089
James Farrington <simbioticman@yahoo.com>
2013-10-29 02:15:49 -0400

    django.contrib.admindocs.middleware
    Removed the global settings.
    Added the uses_settings decorator and kwargs.
    The tests passed.  #unsetting

--- django/contrib/admindocs/middleware.py
+++ django/contrib/admindocs/middleware.py
@@ -1,11 +1,12 @@
-from django.conf import settings
+from django.utils.unsetting import uses_settings
 from django import http
 
 class XViewMiddleware(object):
     """
     Adds an X-View header to internal HEAD requests -- used by the documentation system.
     """
-    def process_view(self, request, view_func, view_args, view_kwargs):
+    @uses_settings({'INTERNAL_IPS':'internal_ips'})
+    def process_view(self, request, view_func, view_args, view_kwargs, internal_ips=()):
         """
         If the request method is HEAD and either the IP is internal or the
         user is a logged-in staff member, quickly return with an x-header
@@ -16,7 +17,7 @@ class XViewMiddleware(object):
             "The XView middleware requires authentication middleware to be "
             "installed. Edit your MIDDLEWARE_CLASSES setting to insert "
             "'django.contrib.auth.middleware.AuthenticationMiddleware'.")
-        if request.method == 'HEAD' and (request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS or
+        if request.method == 'HEAD' and (request.META.get('REMOTE_ADDR') in internal_ips or
                                          (request.user.is_active and request.user.is_staff)):
             response = http.HttpResponse()
             response['X-View'] = "%s.%s" % (view_func.__module__, view_func.__name__)
